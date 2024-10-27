b385a9f42a2b010abcc2cc11de600034d5c1495c
Thej745 <sp132@sciprog.training.ichec.ie>
2024-10-27 11:49:23 +0000

    Practical 5

---
+++ practical5/fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+void fibonacci_step(int *a, int *b) {
+    int next = *a + *b;
+    *a = *b;
+    *b = next;
+}
+
+int main() {
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+	if (n < 1) {
+        printf("Enter a positive integer.\n");
+        exit(1);
+    }
+	int a = 0, b = 1;
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a);
+        fibonacci_step(&a, &b);
+    }
+    return 0;
+}
