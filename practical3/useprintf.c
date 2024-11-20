bb2f39c7e648f80ce8ccb55e056433e038f1a84f
Thej745 <thejratheesh07@gmail.com>
2024-11-20 23:21:23 +0000

    Practical 3

---
+++ practical3/useprintf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	int i1 = 10;
+	int i2 = 200;
+	float fl1 = 1.11;
+	float fl2 = 2.2222;
+
+	printf("I would like the value of i1, that value is %d\n", i1);
+	printf("Two ints %d %d and two floats %.4f %.4f\n",i1,i2,fl1,fl2);
+
+	return 0;
+}
