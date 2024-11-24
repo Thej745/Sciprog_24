49aa362516764802c1d49892534f581b83fe8a73
Thej745 <thejratheesh07@gmail.com>
2024-11-24 11:18:03 +0000

    Practical 9

---
+++ practical9/README.md
@@ -0,0 +1,16 @@
+# main_stub.c
+To compile and run the code run this command
+```{bash}
+gcc main_stub.c -o out
+./out
+```
+## Output
+```
+Enter file name: magic_square.txt
+No. lines, 3
+This is magic
+
+Enter file name: not_magic_square.txt
+No. lines, 3
+This is not Magic
+```
---
+++ practical9/main_stub.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_FILE_NAME 100
+#include "magic_square_stub.h"
+
+int getlines(char filename[MAX_FILE_NAME]);
+
+int main(){  
+
+    //define our file variable
+    FILE *f;  
+    char filename[MAX_FILE_NAME];
+    printf("Enter file name: ");
+    scanf("%s", filename);
+    
+    // ##! n function which gets the number of lines
+    int n = getlines(filename);
+
+    // TODO: Open the file 
+    f = fopen(filename,"r");
+
+    int i;
+    // TODO: Allocating a matrix for storing the magic square
+    // as an array of pointers, where each pointer is a row 
+
+    int** magicsquare =(int**)malloc(n *sizeof(int*));
+    // TODO:inputting integer data into the matrix;
+    for(int i = 0;i<n;i++)
+    {
+        magicsquare[i] = (int*)malloc(n*sizeof(int));
+    }
+    int j;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++){
+            fscanf(f,"%d", &magicsquare[i][j]);
+        }
+    }
+    int isqauremagic = isMagicSquare(magicsquare,n);
+
+    if(isqauremagic ==0){
+        printf("This is not Magic\n");
+    }
+    else{
+        printf("This is magic\n");
+    }
+    // TODO: Freeing each row separately before freeing the array of pointers
+    for(int i =0;i<n;i++){
+    free(magicsquare[i]);
+    }
+    
+    // TODO:Close the file
+    fclose(f);
+
+    return 0;
+}  
+
+//##!
+
+int getlines(char filename[MAX_FILE_NAME]) {
+    FILE *fp;
+    fp = fopen(filename, "r");
+    
+    int ch_read;
+    int count = 0;
+
+    while( (ch_read = fgetc(fp)) != EOF)
+    {
+        if (ch_read == '\n'){
+            count++;
+        }
+    }
+
+    printf("No. lines, %d\n", count);
+    fclose(fp); 
+    return count;
+}
